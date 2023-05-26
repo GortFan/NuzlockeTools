6ed2f789ae12a24b54959fd930f6a793658ea264
Izahe <izahe@udel.edu>
2023-05-25 21:13:01 -0400

    initial setup

--- src/components/gamesections/EncounterTable.js
+++ src/components/gamesections/EncounterTable.js
@@ -1,11 +1,12 @@
 import Card from '../UI/Card'
 import classes from './EncounterTable.module.css'
 
-export default function EncounterTable(props){
+export default function EncounterTable(props){;
+    console.log("props", props);
     return(
         <Card>
             <div className={classes.content}>
-                <h3>{props.title}</h3>
+                <h3>{props.data}</h3>
             </div>
         </Card>
     )
